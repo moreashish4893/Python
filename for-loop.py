5be0584783e16515be6f8f29a9021bc4e4306fe7
moreashish4893 <moreashish4893@gmail.com>
2024-06-26 09:05:16 +0530

    main

--- for-loop.py
+++ for-loop.py
@@ -1,2 +1,12 @@
 for i in range(1,11,1):
     print(i)
+
+###########################################################################
+
+    
+for i in range(1,4):
+    print(i)
+    for j in range(1,4):
+        print(i,j)
+    print(j)
+print(i,j)  
--- while.py
+++ while.py
@@ -1,5 +1,5 @@
 i=1
 while i<=10:
     print(i)
-    i+=1
+    i+=1 
     
